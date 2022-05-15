75cf38c62ee8dcc219917c9cbeb8efa54fbcb3f9
Milad Mohammadi <milad.mo@gmail.com>
2022-05-15 19:15:33 +0000

    added symint node to lowering

--- torch_xla/csrc/ops/ops.h
+++ torch_xla/csrc/ops/ops.h
@@ -264,8 +264,9 @@ torch::lazy::NodePtr Softplus(const XlaValue& input, const XlaValue& beta,
 torch::lazy::NodePtr Selu(const XlaValue& input);
 
 torch::lazy::NodePtr DynamicExpand(const Value& input,
-                                   std::vector<int64_t> upper_bound_size,
-                                   std::vector<bool> dynamic_dim);
+                                   const std::vector<Value>& size_values,
+                                   const std::vector<int64_t> upper_bound_size,
+                                   const std::vector<bool> dynamic_dim);
 
 }  // namespace ops
 }  // namespace ir
